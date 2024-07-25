098fe5e6b7fc0a4dfc1977775778b9f316ed717b
akshay290 <85888610+akshay290@users.noreply.github.com>
2024-07-25 16:37:47 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,54 @@
+// import express from "express";
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+// import userRouter from "./routes/user.route.js";
+// import authRouter from "./routes/auth.route.js";
+// import cookieParser from "cookie-parser";
+// import listingRouter from "./routes/listing.route.js";
+// import path from "path";
+
+// dotenv.config();
+
+// mongoose
+//   .connect("mongodb+srv://Akshay:Akshay29@mern-estate.edxe55y.mongodb.net/?retryWrites=true&w=majority&appName=mern-estate")
+//   .then(() => {
+//     console.log("Connected to MongoDB");
+//   })
+//   .catch((err) => {
+//     console.log(process.env.MONGO);
+//     console.log("Error: " + err);
+//   });
+
+// const __dirname = path.resolve();
+
+// const app = express();
+// app.use(express.json());
+// app.use(cookieParser());
+
+// app.listen(3000, () => {
+//   console.log("Server is running on port 3000");
+// });
+
+// app.use("/api/user", userRouter);
+// app.use("/api/auth", authRouter);
+// app.use("/api/listing", listingRouter);
+
+// app.use(express.static(path.join(__dirname, "/client/dist")));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+// });
+
+// app.use((err, req, res, next) => {
+//   const statusCode = err.statusCode || 500;
+//   const message = err.message || "Internal Server Error";
+//   return res.status(statusCode).json({
+//     success: false,
+//     statusCode,
+//     message,
+//   });
+// });
+
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
@@ -5,39 +56,25 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import listingRouter from "./routes/listing.route.js";
-import path from "path";
 
 dotenv.config();
-
-mongoose
-  .connect("mongodb+srv://Akshay:Akshay29@mern-estate.edxe55y.mongodb.net/?retryWrites=true&w=majority&appName=mern-estate")
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.log(process.env.MONGO);
-    console.log("Error: " + err);
-  });
-
-const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error: " + err);
+  });
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
-
-app.use(express.static(path.join(__dirname, "/client/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
@@ -48,3 +85,5 @@ app.use((err, req, res, next) => {
     message,
   });
 });
+
+export default app;
